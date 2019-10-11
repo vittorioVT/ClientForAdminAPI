5dc8dfb9c7383fde5033fdc7c4b8828150bc0f57
Viktor Tiutiun <vvtiutiun@googlemail.com>
2019-10-11 14:50:04 +0300

    Сreated component header and service entry

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { EntriesComponent } from './entries/entries.component';
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EntriesComponent,
-    FooterComponent
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
