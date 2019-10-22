ebd405e0e08d4345483cfb1b55159ef6222bccf3
Viktor Tiutiun <vvtiutiun@googlemail.com>
2019-10-22 11:52:34 +0300

    Added delete-entry to routes of app

--- src/app/app-router.module.ts
+++ src/app/app-router.module.ts
@@ -3,14 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 //component
 import { EntriesComponent } from './entries/entries.component';
 import { NewEntryComponent } from './new-entry/new-entry.component';
+import { DeleteEntryComponent } from './delete-entry/delete-entry.component';
 import { NgModule } from '@angular/core';
+
 
 //route
 const routes: Routes = [
   { path: '', component: EntriesComponent },
   { path: 'entries', component: EntriesComponent },
-  { path: 'new-entry', component: NewEntryComponent }
-
+  { path: 'new-entry', component: NewEntryComponent },
+  { path: 'delete-entry/:id', component: DeleteEntryComponent}
 ]
 
 @NgModule({
