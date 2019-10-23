b71b2cae6df0ce6733f64d7ff75539aeeb0de4ea
Viktor Tiutiun <vvtiutiun@googlemail.com>
2019-10-23 17:49:27 +0300

    Added html, css and route for component Register

--- src/app/app-router.module.ts
+++ src/app/app-router.module.ts
@@ -4,6 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { EntriesComponent } from './entries/entries.component';
 import { NewEntryComponent } from './new-entry/new-entry.component';
 import { DeleteEntryComponent } from './delete-entry/delete-entry.component';
+import { RegisterComponent } from './register/register.component';
+
 import { NgModule } from '@angular/core';
 
 
@@ -12,7 +14,8 @@ const routes: Routes = [
   { path: '', component: EntriesComponent },
   { path: 'entries', component: EntriesComponent },
   { path: 'new-entry', component: NewEntryComponent },
-  { path: 'delete-entry/:id', component: DeleteEntryComponent}
+  { path: 'delete-entry/:id', component: DeleteEntryComponent },
+  { path: 'register', component: RegisterComponent },
 ]
 
 @NgModule({
