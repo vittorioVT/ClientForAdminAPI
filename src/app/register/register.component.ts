4678d821ececde5525be18b32f855cbfd5368128
Viktor Tiutiun <vvtiutiun@googlemail.com>
2019-10-24 11:06:35 +0300

    Created constructor of class RegisterComponent and onSubmit and matchingFields methods

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
-  constructor() { }
+  registerForm: FormGroup;
 
-  ngOnInit() {
+  constructor(private fb: FormBuilder) {
+    this.registerForm = fb.group({
+      userName: ['', Validators.required],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
+    }, { validator: matchingFields('password', 'confirmPassword') })
+
+  }
+
+  onSubmit() {
+    console.log(this.registerForm.value);
+
+  }
+
+
+}
+    
+function matchingFields(field1, field2) {
+  return form => {
+    if (form.controls[field1].value !== form.controls[field2].value)
+      return { matchingFields: true }
   }
 
 }
